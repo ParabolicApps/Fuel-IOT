0ae4379250022a4b496e48afdf8e1483ca24e20e
masrafi115 <abdurrahmanrafi57@gmail.com>
2026-06-09 12:18:34 +0600

    init

---
+++ IOT-Controller/app/src/main/java/com/xswxm/controller/GuideViewPagerAdapter.java
@@ -0,0 +1,57 @@
+package com.xswxm.controller;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import android.widget.FrameLayout;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import androidx.viewpager2.widget.ViewPager2;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+class GuideViewPagerAdapter extends FragmentStateAdapter {
+    private List<View> views;
+	
+	private Fragment fragment;
+
+    GuideViewPagerAdapter(FragmentManager frag, Lifecycle lifec, List<View> views) {
+        super(frag, lifec);
+        this.views = views;
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        //FrameLayout container = new FrameLayout(context);
+        //container.addView();
+        return new GuideFragment(views.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return views.size();
+    }
+
+    public static class GuideFragment extends Fragment {
+        View view;
+
+        GuideFragment(View view) {
+            this.view = view;
+        }
+
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return view;
+        }
+    }
+}
+
+
---
+++ IOT-Controller/app/src/main/java/com/xswxm/controller/MainActivity.java
@@ -0,0 +1,107 @@
+package com.xswxm.controller;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import com.xswxm.controller.network.Network;
+
+import static android.R.color.holo_blue_light;
+import static com.xswxm.controller.global.Variables.deviceList;
+import static com.xswxm.controller.global.Variables.refreshItem;
+import static com.xswxm.controller.global.Variables.swipeRefreshLayout;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setColorScheme(holo_blue_light);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(true);
+                refreshItem.setEnabled(false);
+                new Network(MainActivity.this).ScanNetwork();
+            }
+        });
+        deviceList = (LinearLayout) findViewById(R.id.deviceList);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        refreshItem = (MenuItem) menu.findItem(R.id.action_refresh);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        Intent intent;
+        //noinspection SimplifiableIfStatement
+        switch (id) {
+            case R.id.action_refresh:
+                swipeRefreshLayout.setRefreshing(true);
+                item.setEnabled(false);
+                new Network(this).ScanNetwork();
+                return true;
+            case R.id.action_add_device:
+                intent = new Intent(this, GuideActivity.class);
+                intent.putExtra("com.xswxm.controller.first_config", true);
+                startActivity(intent);
+                return  true;
+            case R.id.action_fix_device:
+                intent = new Intent(this, GuideActivity.class);
+                intent.putExtra("com.xswxm.controller.first_config", false);
+                startActivity(intent);
+                return  true;
+/*            case R.id.action_settings:
+                return true;*/
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /*
+     * Update device's title (Display Name) if it is available
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (resultCode) {
+            case RESULT_OK:
+                Bundle bundle = data.getExtras();
+                String title = bundle.getString("title");
+                int deviceID = bundle.getInt("deviceID");
+                Device device = (Device) deviceList.findViewById(deviceID);
+                device.setText(title);
+                break;
+            default:
+                break;
+        }
+    }
+
+    /*
+     * Add support for screen rotations.
+     * There are some problems in this guideActivity which needs further developments
+     */
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+}
